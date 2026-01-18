b70d09e7e72f667634bd1f5a92ae635821e3cab7
rakibahmed2153 <rakibahmed412245@gmail.com>
2026-01-18 23:26:21 +0100

    puter implemented

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import {resumes} from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +14,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+
+    const { auth } =  usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
@@ -18,7 +29,6 @@ export default function Home() {
            <h1>Track your Application & Resume Ratings</h1>
            <h2>Review your submissions and check AI-powered feedback </h2>
         </div>
-
         {/*------- Resume cards -------- */}
         {resumes.length > 0 && (
             <div className="resumes-section">
