474ba3dc63d0762ff8703c55428cb084006df1e1
rakibahmed2153 <rakibahmed412245@gmail.com>
2026-01-22 01:18:26 +0100

    Bug fixed for resumw image not showing

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,63 +1,82 @@
-import React, {useEffect, useState} from 'react'
 import {Link, useNavigate, useParams} from "react-router";
+import {useEffect, useState} from "react";
 import {usePuterStore} from "~/lib/puter";
 
-export const Meta = () => ([
-    { title: 'ResumeAI | Review' },
-    { name: 'description', content: 'Details review of the resume' },
+export const meta = () => ([
+    { title: 'ResumeAI | Review ' },
+    { name: 'description', content: 'Detailed overview of your resume' },
 ])
 
-
 const Resume = () => {
-    const {auth, isLoading, fs, kv} = usePuterStore();
+    const { auth, isLoading, fs, kv } = usePuterStore();
     const { id } = useParams();
     const [imageUrl, setImageUrl] = useState('');
     const [resumeUrl, setResumeUrl] = useState('');
-    const [feedback, setFeedback] = useState('');
+    const [feedback, setFeedback] = useState<Feedback | null>(null);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!isLoading && !auth.isAuthenticated) navigate(`/auth?next=/resume/${id}`);
+    }, [isLoading])
 
     useEffect(() => {
         const loadResume = async () => {
             const resume = await kv.get(`resume:${id}`);
 
-            if(!resume) return
+            if(!resume) return;
+
             const data = JSON.parse(resume);
 
             const resumeBlob = await fs.read(data.resumePath);
-            if(!resumeBlob) return
+            if(!resumeBlob) return;
 
-            const pdfBlob = new Blob([resumeBlob], { type: "application/pdf" });
+            const pdfBlob = new Blob([resumeBlob], { type: 'application/pdf' });
             const resumeUrl = URL.createObjectURL(pdfBlob);
             setResumeUrl(resumeUrl);
 
             const imageBlob = await fs.read(data.imagePath);
-            if(!imageBlob) return
-
-            // const pdfBlob = new Blob([imageBlob], { type: "application/pdf" });
+            if(!imageBlob) return;
             const imageUrl = URL.createObjectURL(imageBlob);
             setImageUrl(imageUrl);
 
             setFeedback(data.feedback);
+            console.log({resumeUrl, imageUrl, feedback: data.feedback });
         }
+
         loadResume();
     }, [id]);
 
     return (
         <main className="pt-0!">
-           <nav className="resume-nav">
-               <Link to="/" className="back-button">
-                   <img src="/icons/back.svg" alt="Back" className="w-2.5 h-2.5"/>
-                   <span className="text-gray-800 text-sm font-semibold">Back to Homepage</span>
-               </Link>
-           </nav>
+            <nav className="resume-nav">
+                <Link to="/" className="back-button">
+                    <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
+                    <span className="text-gray-800 text-sm font-semibold">Back to Homepage</span>
+                </Link>
+            </nav>
             <div className="flex flex-row w-full max-lg:flex-col-reverse">
-                <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-screen sticky top-0 items-center justify-center">
-                    {!imageUrl && resumeUrl && (
-                        <div className=" animate-in fade-in duration-1000 gradient-border max-sm:m-0 max-2xl: h-fit w-fit">
-                           <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
-                               <img src={imageUrl} alt="Resume Image" className="w-full h-full object-contain rounded-2xl" title="Resume Image" />
-                           </a>
+                <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-screen sticky top-0 items-center justify-center">
+                    {imageUrl && resumeUrl && (
+                        <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+                            <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
+                                <img
+                                    alt="Image of Resume"
+                                    src={imageUrl}
+                                    className="w-full h-full object-contain rounded-2xl"
+                                    title="resume"
+                                />
+                            </a>
                         </div>
+                    )}
+                </section>
+                <section className="feedback-section">
+                    <h2 className="text-4xl text-black! font-bold">Resume Review</h2>
+                    {feedback ? (
+                        <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
+
+                        </div>
+                    ) : (
+                        <img alt="" src="/images/resume-scan-2.gif" className="w-full" />
                     )}
                 </section>
             </div>
