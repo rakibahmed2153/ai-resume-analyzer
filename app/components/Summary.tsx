2af3ff3bf03648160f91f82de40cc3b62366e08e
rakibahmed2153 <rakibahmed412245@gmail.com>
2026-01-24 23:45:21 +0100

    Resume details full implemented

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ScoreGauge from "~/components/ScoreGauge";
+import ScoreBadge from "~/components/ScoreBadge";
 
 const Category = ({ title, score }: {title: string, score: number}) => {
+    const textColor = score > 70? 'text-green-600' :
+        score > 49? 'text-yellow-600' : 'text-red-600';
     return (
         <div className="resume-summary">
-            {title} - {score}
+            <div className="category">
+                <div className="flex flex-row gap-2 items-center justify-center">
+                    <p className="text-2xl">{title}</p>
+                    <ScoreBadge score={score} />
+                </div>
+                <p className="text-2xl">
+                    <span className={textColor}>{score}</span>
+                </p>
+            </div>
         </div>
     )
 }
